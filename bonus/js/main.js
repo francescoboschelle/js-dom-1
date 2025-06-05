7fe8ccaad14b54254960ab9782bcaf10516f173f
francescoboschelle <francibossch@gmail.com>
2025-06-05 13:52:27 +0200

    Added bonus by implementing main.js to handle lamp on/off logic toggle with event listener

---
+++ bonus/js/main.js
@@ -0,0 +1,26 @@
+// Lamp off: false | Lamp on: true
+let lampStatus = false;
+
+const btn = document.querySelector(".btn");
+const image = document.getElementsByTagName("img")[0];
+const body = document.getElementsByTagName("body")[0];
+
+btn.addEventListener("click", function () {
+  if (lampStatus) {
+    image.src = "./img/white_lamp.png";
+    image.alt = "Lamp off";
+    body.style.backgroundColor = "black";
+    btn.style.color = "white";
+    btn.innerHTML = "Spegni";
+
+    lampStatus = false;
+  } else {
+    image.src = "./img/yellow_lamp.png";
+    image.alt = "Lamp on";
+    body.style.backgroundColor = "white";
+    btn.style.color = "black";
+    btn.innerHTML = "Accendi";
+
+    lampStatus = true;
+  }
+});
